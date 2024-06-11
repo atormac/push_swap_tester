07807606044d0376ba0581500e7692107203962a
atormac <atorma@proton.me>
2024-06-11 03:23:49 +0300

    checker_OS status

--- tester.py
+++ tester.py
@@ -1,18 +1,31 @@
 import subprocess
+from subprocess import PIPE, STDOUT
 import sys
 import random
+import platform
+
+checker = "./checker_"
+if platform.system() == "Linux":
+    checker += "linux"
+else:
+    checker += "Mac"
 
 def test_count(num):
     print("--- " + str(num) + " numbers ---")
     lst = []
     for x in range(0, 10):
-        args = random.sample(range(800), num)
+        args = random.sample(range(-500, 500), num)
         arg_str = ' '.join(map(str, args))
-        stdout = subprocess.run(['./push_swap', arg_str], check=True, capture_output=True, text=True).stdout
+        stdout = subprocess.run(['./push_swap', arg_str],
+                    check=True, capture_output=True, text=True).stdout
         line_count = stdout.count('\n')
-        print(line_count)
         lst.append(line_count)
-    print("--- average: " + str(sum(lst) / len(lst)))
+        p = subprocess.Popen([checker, arg_str], stdout=PIPE, stdin=PIPE,
+                    stderr=PIPE, text=True)
+        checker_status = p.communicate(input=stdout)[0]
+        print(str(line_count) + "\t" + checker_status)
+    avg = sum(lst) / len(lst)
+    print("--- average: " + str(avg) + "\n")
 
 test_count(2)
 test_count(3)
@@ -20,5 +33,5 @@ test_count(4)
 test_count(5)
 test_count(10)
 test_count(100)
-test_count(180)
+test_count(187)
 test_count(500)
